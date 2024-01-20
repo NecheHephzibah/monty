68efd8a96a3bda2ef1c283a1900441cfc9939fb8
NecheHephzibah <neche.bless@gmail.com>
2024-01-20 04:19:28 -0800

    added tests directory and bf directories

---
+++ glo_var.c
@@ -0,0 +1,10 @@
+#include <stddef.h>
+#include "monty.h"
+
+
+/**
+  * Defining global variable here so it can be
+  * accessable across the whole monty directory.
+  */
+
+cmdline_t cmd_args = {NULL, 0, NULL, NULL};
--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-cmdline_t cmd_args = {NULL, 0, NULL, NULL};
 
 void read_file(FILE *file, stack_t **stack);
 instruction_t *find_opcode(char *cmd, instruction_t *cmd_arr);
